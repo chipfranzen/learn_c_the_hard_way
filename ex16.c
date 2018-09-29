c660996498ad7b10970c93bc9e6872fe4df69fff
chipfranzen <chip.franzen@gmail.com>
2018-09-28 22:50:37 -0700

    ex15-16

---
+++ ex16.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Person {
+  char *name;
+  int age;
+  int height;
+  int weight;
+};
+
+struct Person *Person_create(
+    char *name,
+    int age,
+    int height,
+    int weight
+    ) {
+  struct Person *who = malloc(sizeof(struct Person));
+  assert(who != NULL);
+
+  who->name = strdup(name);
+  who->age = age;
+  who->height = height;
+  who->weight = weight;
+
+  return who;
+}
+
+void Person_destroy(struct Person *who)
+{
+  assert(who != NULL);
+  free(who->name);
+  free(who);
+}
+
+void Person_print(struct Person *who)
+{
+  printf("Name: %s\n", who->name);
+  printf("\tAge: %d\n", who->age);
+  printf("\tHeight: %d\n", who->height);
+  printf("\tWeight: %d\n", who->weight);
+}
+
+void Person_print_no_pt(struct Person who)
+{
+  printf("Name: %s\n", who.name);
+  printf("\tAge: %d\n", who.age);
+  printf("\tHeight: %d\n", who.height);
+  printf("\tWeight: %d\n", who.weight);
+}
+
+
+int main(int argc, char *argv[])
+{
+  // make two people structures
+  struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
+
+  struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
+
+  struct Person chip;
+  chip.name = "Chip";
+  chip.age = 30;
+  chip.height = 72;
+  chip.weight = 175;
+
+  printf("%p\n", &chip);
+  Person_print_no_pt(chip);
+
+
+  // print them out and where they are in memory
+  printf("Joe is at memory location %p:\n", joe);
+  Person_print(joe);
+
+  printf("Frank is at memory location %p:\n", frank);
+  Person_print(frank);
+
+  // make everyone age 20 years and print them again
+  joe->age += 20;
+  joe->height -= 2;
+  joe->weight += 40;
+  Person_print(joe);
+
+  frank->age += 20;
+  frank->weight += 20;
+  Person_print(frank);
+
+  // destroy them both so we clean up
+  Person_destroy(frank);
+  Person_destroy(joe);
+
+  return 0;
+}
